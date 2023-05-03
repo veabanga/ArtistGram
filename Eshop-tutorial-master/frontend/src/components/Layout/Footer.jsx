0fe29d4fe448e19f39a723248e8d34bbd2a5a390
Vea Banga <veabanga.vb@gmail.com>
2023-05-03 11:34:04 +0530

    New Changes to frontend

--- Eshop-tutorial-master/frontend/src/components/Layout/Footer.jsx
+++ Eshop-tutorial-master/frontend/src/components/Layout/Footer.jsx
@@ -16,7 +16,7 @@ import {
 const Footer = () => {
   return (
     <div className="bg-[#000] text-white">
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
+      {/* <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
         <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
           <span className="text-[#56d879]">Subscribe</span> us for get news{" "}
           <br />
@@ -33,8 +33,8 @@ const Footer = () => {
           <button className="bg-[#56d879] hover:bg-teal-500 duration-300 px-5 py-2.5 rounded-md text-whie md:w-auto w-full">
             Submit
           </button>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
       <div className="grid grid-cols-1 sm:gird-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16 sm:text-center">
         <ul className="px-5 text-center sm:text-start flex sm:block flex-col items-center">
           <img
--- Eshop-tutorial-master/frontend/src/components/Route/Hero/Hero.jsx
+++ Eshop-tutorial-master/frontend/src/components/Route/Hero/Hero.jsx
@@ -13,9 +13,9 @@ const Hero = () => {
     >
       <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
         <h1
-          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#ffffff] font-[600] capitalize`}
         >
-          Best Collection for <br /> home Decoration
+          Best Collection of <br /> Unseen Art
         </h1>
         <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
